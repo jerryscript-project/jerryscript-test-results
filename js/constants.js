a7fee48c3fb8696af6248c2df69939bfd18d4754
Daniella Barsony <shinygoomy@gmail.com>
2019-04-30 09:42:20 +0200

    Remove Artik053 target (#19)

--- js/constants.js
+++ js/constants.js
@@ -2,11 +2,9 @@ const views = [
   'home',
   'stm32f4dis',
   'rpi2',
-  'artik053'
 ];
 
 const devices = {
-  artik053: 'ARTIK 053',
   rpi2: 'Raspberry Pi 2',
   stm32f4dis: 'STM32F4-Discovery'
 };
